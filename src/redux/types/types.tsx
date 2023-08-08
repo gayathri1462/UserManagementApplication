58f55a9958c78b3aa98ee3ce4b8a2ab8d4321529
Gayathri Devi Nagalapuram <gayathri1462@gmail.com>
2023-08-08 09:23:02 +0530

    Update types.tsx

--- src/redux/types/types.tsx
+++ src/redux/types/types.tsx
@@ -3,7 +3,6 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { RootState, AppDispatch } from "../store";
 
 // Action Types
-export const UPDATE_USERS = "UPDATE_USERS";
 export const ADD_USER = "ADD_USER";
 export const EDIT_USER_BY_ID = "EDIT_USER_BY_ID";
 export const DELETE_USER_BY_ID = "DELETE_USER_BY_ID";
